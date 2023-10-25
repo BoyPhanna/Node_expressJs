c1b9741356cf7574b009615dc812b79297597e10
BoyPhanna <kimphanna56@gmail.com>
2023-10-25 22:43:24 +0700

    http Server

--- app.js
+++ app.js
@@ -1,18 +1,7 @@
-const fs=require ('fs')
-
-fs.readFile('myFile/input.txt','utf-8',(err,data)=>{
-    if(err)
-        console.log("Some thing have wrong!")
-    else{
-        const outputText=`Hello node js 2\n${data}\nFile wroted ${new Date()}`
-        fs.writeFile('myFile/ouput2.txt',outputText,err=>{
-            if(err){
-                console.log("Somet thing have wrong!")
-            }
-            else{
-                console.log("File Wroted");
-            }
-        })
-    }
+const http=require ('http')
+const server=http.createServer((req,res)=>{
+    res.write("<h1>Hello phanna</h1>")
+    res.end()
+}).listen(3000,()=>{
+    console.log("start server port 3000")
 })
-
