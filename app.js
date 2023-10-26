4eb601d0777117f0888feb8316987806c4f7c828
BoyPhanna <kimphanna56@gmail.com>
2023-10-26 19:37:24 +0700

    URL Respons And Request

--- app.js
+++ app.js
@@ -1,7 +1,20 @@
 const http=require ('http')
 const server=http.createServer((req,res)=>{
-    res.write(`<h1 style="color: blue;">Hello phanna</h1>`)
-    res.end()
+    const parthName=req.url
+    console.log("url : "+parthName)
+    if(parthName=="/" || parthName=="/home"){
+        const myPage=`<h1 style="color: blue;">Hello Home Page phanna22</h1>`
+        res.end(myPage)
+    }
+    if( parthName=="/product"){
+        const myPage=`<h1 style="color: blue;">Hello Product</h1>`
+        res.end(myPage)
+    }
+    else{
+        res.writeHead(404)
+        const myPage=`<h1 style="color: blue;">404 Not Fount</h1>`
+        res.end(myPage)
+    }
 }).listen(3000,()=>{
     console.log("start server port 3000")
 })
