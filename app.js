c785a6a3daf56540d6c3db46595c8481a4377854
BoyPhanna <kimphanna56@gmail.com>
2023-10-26 19:21:20 +0700

    nodemon npm start

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const http=require ('http')
 const server=http.createServer((req,res)=>{
-    res.write("<h1>Hello phanna</h1>")
+    res.write(`<h1 style="color: blue;">Hello phanna</h1>`)
     res.end()
 }).listen(3000,()=>{
     console.log("start server port 3000")
